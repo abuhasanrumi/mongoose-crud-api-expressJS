f018dc08e21f71c68e263c5ba96cc208facd836b
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-02-03 21:40:45 +0600

    basic crud operation using Mongoose done

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -6,12 +6,33 @@ const Todo = new mongoose.model("Todo", todoSchema)
 
 // Get all the todos
 router.get('/', async (req, res) => {
+    await Todo.find({ status: 'active' }, (err, data) => {
+        if (err) {
+            res.status(500).json({
+                error: "Server side error"
+            })
+        } else {
+            res.status(200).json({
+                result: data
+            })
+        }
+    }).clone()
 
 })
 
 // Get a todo by id
 router.get('/:id', async (req, res) => {
-
+    await Todo.find({ _id: req.params.id }, (err, data) => {
+        if (err) {
+            res.status(500).json({
+                error: "Your input is not supported or invalid"
+            })
+        } else {
+            res.status(200).json({
+                result: data
+            })
+        }
+    }).clone()
 })
 
 // post a todo
@@ -47,12 +68,39 @@ router.post('/all', async (req, res) => {
 
 // put todo
 router.put('/:id', async (req, res) => {
-
+    await Todo.findByIdAndUpdate({ _id: req.params.id }, {
+        $set: {
+            status: "active"
+        }
+    }, {
+        new: true,
+        useFindAndModify: false
+    }, (err) => {
+        if (err) {
+            res.status(500).json({
+                error: "Your input is not supported or invalid"
+            })
+        } else {
+            res.status(200).json({
+                message: "To do has been updated"
+            })
+        }
+    }).clone()
 })
 
 // delete todo
 router.delete('/:id', async (req, res) => {
-
+    await Todo.deleteOne({ _id: req.params.id }, (err) => {
+        if (err) {
+            res.status(500).json({
+                error: "Your input is not supported or invalid"
+            })
+        } else {
+            res.status(200).json({
+                message: "To do was deleted successfully"
+            })
+        }
+    }).clone()
 })
 
 module.exports = router
